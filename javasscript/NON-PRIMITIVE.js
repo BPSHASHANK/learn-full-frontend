e352a600c8cb1c50a5265a81e72ef3e0d5a0e6cd
B P SHASHANK <154719115+BPSHASHANK@users.noreply.github.com>
2024-06-27 14:01:04 +0530

    Add files via upload

---
+++ javasscript/NON-PRIMITIVE.js
@@ -0,0 +1,23 @@
+let a=["shashnak","vinod","abcd"]
+console.log(typeof(a))
+
+
+
+//objects which enclose with {}
+
+let q={
+    name:"shashank",
+    age:22
+}
+console.log(q)
+
+let b=function(){
+    console.log("helloworld");
+}
+
+b()
+
+
+{
+
+}
