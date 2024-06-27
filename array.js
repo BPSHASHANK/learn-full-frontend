f4e361c8fc6a7e4786e2dcd78c63d8f4836cb0a2
B P SHASHANK <154719115+BPSHASHANK@users.noreply.github.com>
2024-06-27 18:06:16 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,24 @@
+//array is noting but used for stored for string num etc every thing
+let arr=new Array("shashank","ram","vinod")
+console.log(arr)
+
+//or
+
+let arr1=["shashank","ram","vinod"]
+console.log(arr1)
+console.log(arr1[0])
+
+//methods in an array
+arr1.push("bheem")
+console.log(arr1)
+arr1.pop()
+console.log(arr1)
+arr1.unshift("krishna")
+console.log(arr1)
+arr1.shift("krishna")
+console.log(arr1)
+console.log(arr1.indexOf("ram"))
+console.log(arr1.includes("ram")) 
+console.log(arr1.slice(0,1))
+console.log(arr1.splice(0,1))
+
