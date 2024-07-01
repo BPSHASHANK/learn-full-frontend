630c9659d9c424ff7454bfaed72bbaf3a1f527c0
B P SHASHANK <154719115+BPSHASHANK@users.noreply.github.com>
2024-07-01 14:44:23 +0530

    Add files via upload

---
+++ javasscript/day7/switch.js
@@ -0,0 +1,20 @@
+let month="march"
+switch (month) {
+    
+    case "jan":
+        console.log("jan")
+        break;
+    case "feb":
+        console.log("feb")
+        break;
+    case "march":
+        console.log("mar")
+        break;
+    case" apr":
+        console.log("april")
+        break;
+
+    default:
+        console.log("defualt")
+        break;
+}
